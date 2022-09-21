59d921103962f1a568466fe97521202f6141c67c
Lokesh8317 <112701965+Lokesh8317@users.noreply.github.com>
2022-09-21 10:02:50 +0530

    Add files via upload

---
+++ voting in c++.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+void main()
+{
+       int age;
+       clrscr();
+       cout<<"Enter Age : ";
+       cin>>age;
+       if(age> 17)
+       cout<<"Eligible for voting";
+       getch();
+}
